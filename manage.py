01a07848bcf2687dd7dbe4163fd5efda81f91e09
emil <amionv99@gmail.com>
2022-04-21 13:35:23 +0300

    :bug: fix bug with manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,19 @@
 import uvicorn
-from app.internal.drivers.fast_api import FastAPIServer
+from fastapi import FastAPI
 
-app = FastAPIServer.get_app()
+from conf.server import DEBUG, TITLE_API, DESCRIPTION_API, VERSION_API
+from internal.web.api.general import general_router
 
+app = FastAPI(
+    debug=DEBUG,
+    title=TITLE_API,
+    description=DESCRIPTION_API,
+    version=VERSION_API,
+    docs_url='/swagger',
+    openapi_url='/openapi.json'
+)
+
+app.include_router(general_router)
 
 if __name__ == "__main__":
     uvicorn.run("manage:app", host="0.0.0.0", port=8080, log_level="info")
